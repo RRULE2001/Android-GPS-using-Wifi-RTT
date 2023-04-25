c1e4593eccbafbb4e36fd213b5bbacde9af14e06
bnewman122008 <119713257+bnewman122008@users.noreply.github.com>
2023-04-25 15:23:59 -0400

    Added lookup table for obtaining router's X and Y position based on MAC Address. Fixed some minor warnings. Added doxygen comments for some functions and variables.

--- WiFi RTT Check/app/src/main/java/com/example/tes_wifi_rtt/GPSCoreAPI.java
+++ WiFi RTT Check/app/src/main/java/com/example/tes_wifi_rtt/GPSCoreAPI.java
@@ -17,16 +17,16 @@
 /** TODO:
  * Error check necessary variables.
  * Exceptions?
- * Store router X and Y position on object creation
- *  -Lookup table?
  */
 
 package com.example.tes_wifi_rtt;
 
 /* Imports */
 import java.lang.String;
+import java.util.HashMap;
+import java.util.Arrays;
 
-/** \class GPSCoreAPI
+/**
  *  \brief This class contains methods and sub-classes for general GPS
  *  data storage and conversion.
  *
@@ -39,10 +39,11 @@ import java.lang.String;
 public class GPSCoreAPI {
     /* Public Variables */
     /* Private Variables */
-    private Device device;      /*!< Object containing information for a single device to record on the GPS. */
+    private final Device device;                        /*!< Object containing information for a single device to record on the GPS. */
+    private final HashMap<String, int[]>lookupTable;    /*!< HashMap containing a list of MAC Addresses and their corresponding X and Y coordinates */
 
     /* Sub-Classes */
-    /** \class Router
+    /**
      *  \brief This class contains data relevant to a single router.
      *
      *  Each router object stores relevant data for a single router
@@ -50,7 +51,7 @@ public class GPSCoreAPI {
      *  obtaining data are provided. The class is private to the core
      *  GPSCoreAPI clas in order to abstract information and processes.
      */
-    private class Router {
+    private static class Router {
         /* Public Variables */
         /* Private Variables */
         private int x;          /*!< X coordinate of router on cartesian grid. */
@@ -61,6 +62,11 @@ public class GPSCoreAPI {
 
         /* Constructor(s) */
 
+        /**
+         *  \brief Empty constructor. Initializes all variables as null.
+         *
+         *  \param None.
+         */
         public Router() {
             this.x = 0;
             this.y = 0;
@@ -69,6 +75,15 @@ public class GPSCoreAPI {
             this.rssi = 0;
         }
 
+        /**
+         *  \brief Constructor for the class Router. Initializes all variables with the input parameters.
+         *
+         *  \param x An integer containing the X position of the router on a cartesian grid.
+         *  \param y An integer containing the Y position of the router ona cartesian grid.
+         *  \param dist An integer containing the distance in millimeters(mm) from a device.
+         *  \param MACAddr A String object of the router's MAC Address.
+         *  \param rssi An integer containing the signal strength of the router to a device.
+         */
         public Router(int x, int y, int dist, String MACAddr, int rssi) {
             this.x = x;
             this.y = y;
@@ -79,44 +94,106 @@ public class GPSCoreAPI {
 
         /* Gets */
 
+        /**
+         *  \brief Gets the current X position on a cartesian grid of the router.
+         *
+         *  \param None.
+         *  \return An integer representing the X position on a cartesian grid.
+         */
         public int getX() {
             return this.x;
         }
 
+        /**
+         *  \brief Gets the current Y position on a cartesian grid of the router.
+         *
+         *  \param None.
+         *  \return An integer representing the Y position on a cartesian grid.
+         */
         public int getY() {
             return this.y;
         }
 
+        /**
+         *  \brief Gets the current distance in millimeters(mm) from a device.
+         *
+         *  \param None.
+         *  \return An integer representing the distance from the parent device in millimeters(mm).
+         */
         public int getDist() {
             return this.dist;
         }
 
+        /**
+         *  \brief Gets the router's current MAC Address.
+         *
+         *  \param None.
+         *  \return A String object containing the MAC Address of the router.
+         */
         public String getMACAddr() {
             return this.MACAddr;
         }
 
+        /**
+         *  \brief Gets the current signal strength to the parent device. Represented as a negative
+         *         number.
+         *
+         *  \param None.
+         *  \return An integer representing the signal strength to the parent device.
+         */
         public int getRSSI() {
             return this.rssi;
         }
 
         /* Sets */
 
+        /**
+         *  \brief Sets the router's X position on a cartesian grid.
+         *
+         *  \param x An integer containing the X position on a cartesian grid.
+         *  \return None.
+         */
         public void setX(int x) {
             this.x = x;
         }
 
+        /**
+         *  \brief Sets the router's Y position on a cartesian grid.
+         *
+         *  \param y An integer containing the Y position on a cartesian grid.
+         *  \return None.
+         */
         public void setY(int y) {
             this.y = y;
         }
 
+        /**
+         *  \brief Sets the router's distance from the parent device in millimeters(mm).
+         *
+         *  \param dist An integer containing the distance in millimeters(mm) from the parent device.
+         *  \return None.
+         */
         public void setDist(int dist) {
             this.dist = dist;
         }
 
+        /**
+         *  \brief Sets the router's MAC Address.
+         *
+         *  \param MACAddr A String object containing the MAC Address.
+         *  \return None.
+         */
         public void setMACAddr(String MACAddr) {
             this.MACAddr = MACAddr;
         }
 
+        /**
+         *  \brief Sets the router's signal strength from the parent device. Input should always be
+         *         a number below, or equal to zero, i.e. negative.
+         *
+         *  \param rssi An integer containing the signal strength from the parent device.
+         *  \return None.
+         */
         public void setRSSI(int rssi) {
             this.rssi = rssi;
         }
@@ -128,15 +205,20 @@ public class GPSCoreAPI {
          *  object and outputs whether the objects are equal.
          *
          *  \param[in] o An Object to compare this object to
-         *  \return Returns true if the objects are equal, else
-         *  returns false.
+         *  \return Returns true if the objects or MAC Addresses are equal.
          */
         public boolean equals(Object o) {
-            return (this == o);
+            if (this == o)
+                return true;
+            else if (o instanceof Router) {
+                Router temp = (Router)o;
+                return (this.getMACAddr().equals(temp.getMACAddr()));
+            }
+            return false;
         }
     }
 
-    /** \class Device
+    /**
      *  \brief This class contains information relevant to a single device that
      *  will be used in the GPS.
      *
@@ -144,21 +226,33 @@ public class GPSCoreAPI {
      *  is stored inside this class. It is designed for scalability to make adding
      *  additional devices to the GPS easier while abstracting out unnecessary information.
      */
-    private class Device {
+    private static class Device {
         /* Public Variables */
         /* Private Variables */
-        private int x;
-        private int y;
-        private Router[] routerList;
+        private int x;                  /*!< The X position on a cartesian grid. */
+        private int y;                  /*!< The Y position on a cartesian grid. */
+        private Router[] routerList;    /*!< Contains a list of routers used for triangulating the parent devices position. Sorted by RSSI then distance. */
 
         /* Constructor(s) */
 
+        /**
+         *  \brief Empty constructor. Initializes all variables to null.
+         *
+         *  \param None.
+         */
         public Device() {
             this.x = 0;
             this.y = 0;
             this.routerList = null;
         }
 
+        /**
+         *  \brief Constructor for the class Device. Initializes all variables with the input data.
+         *
+         *  \param x An integer containing the X position on a cartesian grid.
+         *  \param y An integer containing the Y position on a cartesian grid.
+         *  \param routerList A list of routers used for triangulating the parent devices position.
+         */
         public Device(int x, int y, Router[] routerList) {
             this.x = x;
             this.y =y;
@@ -204,19 +298,35 @@ public class GPSCoreAPI {
          *  false.
          */
         public boolean equals(Object o) {
-            return (this == o);
+            if (this == o)
+                return true;
+            else if (o instanceof Device) {
+                Device temp = (Device)o;
+                return ((this.getX() == temp.getX()) & (this.getY() == temp.getY()) & (Arrays.equals(this.getRouterList(), temp.getRouterList())));
+            }
+            return false;
         }
     }
 
     /* Constructor(s) */
 
-    public GPSCoreAPI() { this.device = new Device(); }
+    public GPSCoreAPI() {
+        this.device = new Device();
+        this.lookupTable = new HashMap<>();
+        lookupTable.put("70:3a:cb:6e:ce:85", new int[] {0, 0});
+        lookupTable.put("70:3a:cb:29:4b:3a", new int[] {1, 1});
+        lookupTable.put("d8:6c:63:d6:5f:aa", new int[] {2, 2});
+    }
 
     public GPSCoreAPI(int dist, String MACAddr, int rssi) {
         // Create a new array of routers with one element
         Router[] router = {new Router(0, 0, dist, MACAddr, rssi)};
         // Create new Device object with input router data
         this.device = new Device(0, 0, router);
+        this.lookupTable = new HashMap<>();
+        lookupTable.put("70:3a:cb:6e:ce:85", new int[] {0, 0});
+        lookupTable.put("70:3a:cb:29:4b:3a", new int[] {1, 1});
+        lookupTable.put("d8:6c:63:d6:5f:aa", new int[] {2, 2});
     }
 
     /* Gets */
@@ -410,7 +520,7 @@ public class GPSCoreAPI {
             // If MAC ID is found then set X and Y position
             if (router.getMACAddr().equals(MACAddr)) {
                 router.setX(x);
-                router.setY(x);
+                router.setY(y);
                 break;
             }
         }
@@ -438,7 +548,7 @@ public class GPSCoreAPI {
         if (routerList != null) {
             // Iterate through router list to see if input MAC address already exists
             for (int i = 0; i < routerList.length; i++) {
-                // If MAC address is found then delete current object
+                // If MAC address is found then delete current object and shift remaining objects
                 if (routerList[i].getMACAddr().equals(MACAddr)) {
                     for(int k = i; k < routerList.length - 1; k++){
                         routerList[k] = routerList[k + 1];
@@ -448,16 +558,24 @@ public class GPSCoreAPI {
                     break;
                 }
             }
-            // Create new router list with original size + 1
+            // If a new object is being inserted then create new router list with size + 1
             if(isNew)
                 newRouterList = new Router[routerList.length + 1];
+            // Else current object is just being updated so make new list of the same size
             else
                 newRouterList = new Router[routerList.length];
 
             // Copies over past data
             System.arraycopy(routerList, 0, newRouterList, 0, routerList.length);
 
-            newRouterList[newRouterList.length - 1] = new Router(0, 0, dist, MACAddr, rssi); // Adds to end of array
+            // Search for the input MAC Address in the lookup table
+            int[] routerPos = lookupTable.get(MACAddr);
+            // If the MAC Address was found in the lookup table then create object with the defined positional data
+            if (routerPos != null)
+                newRouterList[newRouterList.length - 1] = new Router(routerPos[0], routerPos[1], dist, MACAddr, rssi);
+            // Else MAC Address was not found in the lookup table so create object with no positional data
+            else
+                newRouterList[newRouterList.length - 1] = new Router(0, 0, dist, MACAddr, rssi);
 
             for (int i = 0; i < newRouterList.length; i++) {
                 for (int j = i; j < newRouterList.length - 1; j++) {
@@ -478,7 +596,15 @@ public class GPSCoreAPI {
         } else {
             // Router list is empty so create a new list
             newRouterList = new Router[1];
-            newRouterList[0] = new Router(0, 0, dist, MACAddr, rssi);
+
+            // Search for the input MAC Address in the lookup table
+            int[] routerPos = lookupTable.get(MACAddr);
+            // If the MAC Address was found in the lookup table then create object with the defined positional data
+            if (routerPos != null)
+                newRouterList[0] = new Router(routerPos[0], routerPos[1], dist, MACAddr, rssi);
+            // Else MAC Address was not found in the lookup table so create object with no positional data
+            else
+                newRouterList[0] = new Router(0, 0, dist, MACAddr, rssi);
         }
         this.device.setRouterList(newRouterList);
     }
@@ -498,7 +624,6 @@ public class GPSCoreAPI {
             GPSCoreAPI temp = (GPSCoreAPI)o;
             return (this.device == temp.device);
         }
-
         return false;
     }
 }
